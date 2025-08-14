37f76f85cf2587a7a7fecbbea097f2e1f2658173
Leonardo Fernandes Sousa <suigamer155@gmail.com>
2025-08-14 21:29:11 +0100

    simplificação do main com a função get_command_executer

--- src/main.rs
+++ src/main.rs
@@ -10,49 +10,44 @@ use commands::Command;
 
 
 fn main() -> std::io::Result<()> {
-    
     clearscreen::clear().expect("failed to clear screen");
-
     let args: Vec<String> = env::args().collect();
-    
     if args.len() < 2 {
         println!("Please pass arguments to the command. Use 'help' to see the list of commands");
         return Ok(());
     }
-
-    let executer: Box<dyn Command> = match args[1].as_str() {
-        "init" => Box::new(commands::InitCommand{}),
-        "clear" => Box::new(commands::ClearCommand{}),
-        "reset" => Box::new(commands::ResetCommand{}),
-        "help" => Box::new(commands::HelpCommand{}),
-        "show" => Box::new(commands::ShowCommand::new(&args)),
-        "add" => match commands::AddCommand::new(&args) {
-            Ok(command) => Box::new(command),
-            Err(e) => { println!("{}", e); return Ok(()); }
-        },
-        "toggle" => match commands::ToggleCommand::new(&args) {
-            Ok(command) => Box::new(command),
-            Err(e) => { println!("{}", e); return Ok(()); }
-        },
-        "remove" => match commands::RemoveCommand::new(&args) {
-            Ok(command) => Box::new(command),
-            Err(e) => { println!("{}", e); return Ok(()); }
-        },
-        "daltonic" => match commands::DaltonicCommand::new(&args) {
-            Ok(command) => Box::new(command),
-            Err(e) => { println!("{}", e); return Ok(()); }
-        },
-        "edit" => match commands::EditCommand::new(&args) {
-            Ok(command) => Box::new(command),
-            Err(e) => { println!("{}", e); return Ok(()); }
-        },
-        "swap" => match commands::SwapCommand::new(&args) {
-            Ok(command) => Box::new(command),
-            Err(e) => { println!("{}", e); return Ok(()); }
-        },
-        _ => Box::new(commands::InvalidCommand{}),
+    let executer = match get_command_executer(&args) {
+        Ok(cmd) => cmd,
+        Err(e) => {
+            println!("{}", e);
+            return Ok(());
+        }
     };
     let todo_file = ToDoFile::new(String::from("./todo.json"));
     executer.execute(&todo_file);
     Ok(())
+}
+
+
+fn get_command_executer(args: &Vec<String>) -> Result<Box<dyn Command>, String> {
+    match args[1].as_str() {
+        "init"     => Ok(Box::new(commands::InitCommand {})),
+        "clear"    => Ok(Box::new(commands::ClearCommand {})),
+        "reset"    => Ok(Box::new(commands::ResetCommand {})),
+        "help"     => Ok(Box::new(commands::HelpCommand {})),
+        "show"     => Ok(Box::new(commands::ShowCommand::new(&args))),
+        "add"      => commands::AddCommand::new(&args)
+                        .map(|cmd| Box::new(cmd) as Box<dyn Command>),
+        "toggle"   => commands::ToggleCommand::new(&args)
+                        .map(|cmd| Box::new(cmd) as Box<dyn Command>),
+        "remove"   => commands::RemoveCommand::new(&args)
+                        .map(|cmd| Box::new(cmd) as Box<dyn Command>),
+        "daltonic" => commands::DaltonicCommand::new(&args)
+                        .map(|cmd| Box::new(cmd) as Box<dyn Command>),
+        "edit"     => commands::EditCommand::new(&args)
+                        .map(|cmd| Box::new(cmd) as Box<dyn Command>),
+        "swap"     => commands::SwapCommand::new(&args)
+                        .map(|cmd| Box::new(cmd) as Box<dyn Command>),
+        _ => Ok(Box::new(commands::InvalidCommand {})),
+    }
 }
